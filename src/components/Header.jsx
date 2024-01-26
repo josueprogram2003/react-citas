d5238900f3131097398c92e427076013291048fc
josueprogram2003 <73667027+josueprogram2003@users.noreply.github.com>
2024-01-26 12:45:36 -0500

    123

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header (props){
 
   return(
-    <h1 className="font-black lg:text-4xl 2xl:text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientees {""}
+    <h1 className="font-black lg:text-4xl 2xl:text-5xl text-center md:w-2/3 mx-auto">Seguimiento Pacientes con GITHUB {""}
     <span className="text-indigo-600">Veterinaria</span>
     </h1>
   )
